56f62af232cb55c13f71c8b2a164c71a7ce089a4
Vladislav Momatau <vladislav.momatau@itechart-group.com>
2022-01-24 23:26:14 +0200

    Lesson 3 content

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,7 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import 'bootstrap'
+import "src/profile.js"
 
 Rails.start()
 Turbolinks.start()
